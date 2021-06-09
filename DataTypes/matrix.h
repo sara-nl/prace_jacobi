cc39e18ac64c362b1dfa2e71c9d17c038aed9a7b
Maxim Masterov <maxim.vadimovich@gmail.com>
2021-06-09 18:26:17 +0200

    Added final code of the 1st day

--- DataTypes/matrix.h
+++ DataTypes/matrix.h
@@ -96,7 +96,8 @@ public:
     }
 
     /*!
-     * @brief Return number of local rows.
+     * @brief Return number of local rows including rows which
+     *        correspond to halo elements.
      * @note If this method is called for the object of inherited \e Field class,
      * the method will return the number of elements in i-th direction.
      */
