efcf64469d8773f33efa60aa7ea186ac8a41cc3c
maxim-masterov <maxim.vadimovich@gmail.com>
2024-05-22 13:15:29 +0200

    Update copyright notes

--- DataTypes/vector.cpp
+++ DataTypes/vector.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Maksim Masterov, SURF
+ * Copyright (c) 2024 Maksim Masterov, SURF
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
